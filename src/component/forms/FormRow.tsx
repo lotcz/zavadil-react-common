9acb6fa445266b33e80048c4bf18464475ca0a94
Karel Zavadil <zavadil@incomaker.com>
2026-03-10 21:58:56 +0100

    form row control

--- src/component/forms/FormRow.tsx
+++ src/component/forms/FormRow.tsx
@@ -1,18 +1,16 @@
-import {PropsWithChildren, useMemo} from "react";
+import {PropsWithChildren} from "react";
 import {Form} from "react-bootstrap";
 import {Localize} from "../localization";
-import {StringUtil} from "zavadil-ts-common";
 
 export type FormRowProps = PropsWithChildren & {
 	label: string;
-	id?: string;
+	forId?: string;
 };
 
-export function FormRow({id, label, children}: FormRowProps) {
-	const actualId = useMemo(() => id || StringUtil.randomString(), [id]);
+export function FormRow({forId, label, children}: FormRowProps) {
 	return (
 		<Form.Group>
-			<Form.Label id={actualId}>
+			<Form.Label htmlFor={forId}>
 				<Localize text={label}/>
 			</Form.Label>
 			<div>{children}</div>
--- src/component/forms/FormRowControl.tsx
+++ src/component/forms/FormRowControl.tsx
@@ -1,15 +1,17 @@
 import {Form, FormControlProps} from "react-bootstrap";
 import {FormRow} from "./FormRow";
+import {useMemo} from "react";
+import {StringUtil} from "zavadil-ts-common";
 
 export type FormRowControlProps = FormControlProps & {
 	label: string;
-	id?: string;
 };
 
 export function FormRowControl(props: FormRowControlProps) {
+	const actualId = useMemo(() => props.id || StringUtil.randomString(), [props]);
 	return (
-		<FormRow label={props.label} id={props.id}>
-			<Form.Control {...props}/>
+		<FormRow label={props.label} forId={actualId}>
+			<Form.Control {...props} id={actualId}/>
 		</FormRow>
 	);
 }
